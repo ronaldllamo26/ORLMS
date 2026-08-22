80a7ab84470037edbdd7083f2057d210884a74be
ronaldllamo26 <llamoronald1@gmail.com>
2026-08-23 07:53:34 +0800

    style(api): pretty-print JSON responses with unescaped slashes

--- core/Controller.php
+++ core/Controller.php
@@ -126,8 +126,8 @@ class Controller
     protected function json(array $data, int $statusCode = 200): void
     {
         http_response_code($statusCode);
-        header('Content-Type: application/json');
-        echo json_encode($data);
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
         exit;
     }
 
